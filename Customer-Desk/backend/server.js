f08a235ac171bb9fac6b454d111141d648067a41
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-05-02 17:23:32 +0530

    Update server.js

--- Customer-Desk/backend/server.js
+++ Customer-Desk/backend/server.js
@@ -14,7 +14,11 @@ dotenv.config();
 // App configuration
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server, {
+  cors: 
+  { origin: ["http://localhost:5173", "https://frontend-31u7.onrender.com"],
+   methods: ["GET", "POST"],
+  } });
 const port = process.env.PORT || 5000;
 
 // Middleware
